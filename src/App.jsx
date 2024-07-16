12faa96463bfa7cc3733847a2122385cd729c356
Zuild <zalgarme@gmail.com>
2024-07-16 17:39:50 +0800

    react

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+      <h1>First React App Installation</h1>
+      <h5>What is React?</h5>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt at possimus, ut recusandae maxime tempore ea mollitia iste nobis in fuga esse distinctio magni quis aspernatur exercitationem fugit odio architecto. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat vero deleniti distinctio dolorem cupiditate at possimus, quam similique veniam a aut facere ab laudantium harum voluptatibus voluptas necessitatibus inventore dolore.
+      </p>
+
+      <ol>
+        <li>Junrih</li>
+        <li>Zean</li>
+
+        <p>
+          <b>Note:</b>Make sure to change the Web App Name that appears in Browser Tab
+        </p>
+      </ol>
+    </div>
+        
+    </div>
+  )
+}
+
+export default App;
